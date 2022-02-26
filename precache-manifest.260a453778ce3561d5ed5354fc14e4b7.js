00c9d310db40e572009627ac764f871b29dd9e21
rohitnalat <rohitnalat4418@gmail.com>
2022-02-26 16:59:38 +0530

    Updates

---
+++ precache-manifest.260a453778ce3561d5ed5354fc14e4b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7cc7ae766bc955f69ebe",
+    "url": "/TextUtils/static/css/main.f0a7f59a.chunk.css"
+  },
+  {
+    "revision": "7cc7ae766bc955f69ebe",
+    "url": "/TextUtils/static/js/main.7cc7ae76.chunk.js"
+  },
+  {
+    "revision": "db20f86d0530543db744",
+    "url": "/TextUtils/static/js/1.db20f86d.chunk.js"
+  },
+  {
+    "revision": "3a2e0e0b763789a1abbb",
+    "url": "/TextUtils/static/js/2.3a2e0e0b.chunk.js"
+  },
+  {
+    "revision": "ae1ab5972405ba779003",
+    "url": "/TextUtils/static/js/runtime~main.ae1ab597.js"
+  },
+  {
+    "revision": "7dfde032b0545351488be3c03f7be3b0",
+    "url": "/TextUtils/index.html"
+  }
+];
